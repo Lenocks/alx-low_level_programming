9efb7a00750f94cd04d4e2100aeb8b7c174474e8
Lenocks <werelenocks@gmail.com>
2022-04-14 06:44:11 +0300

    104-fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -35,7 +35,6 @@ if (fib1_half2 + fib2_half2 > 99999999999)
 half1 += 1;
 half2 %= 10000000000;
 }
-
 printf("%lu%lu", half1, half2);
 if (count != 98)
 printf(",");
@@ -45,7 +44,7 @@ fib1_half2 = fib2_half2;
 fib2_half1 = half1;
 fib2_half2 = half2;;
 }
-
 printf("\n");
 return (0);
+
 }
