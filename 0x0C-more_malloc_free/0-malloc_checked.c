203d43cb5800885831013a017933fa99b9ac9d3f
Lenocks <werelenocks@gmail.com>
2022-05-11 01:55:16 +0300

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * malloc_checked - allocates memory using malloc
- * @b: size of memory to be allocated
- * Return: pointer to the allocated memory (SUCCESS)
- * 98 if insufficient memory was available (FAILURE)
+ *malloc_checked - allocates memory using malloc
+ *@b: size of memory to be allocated
+ *Return: pointer to the allocated memory (SUCCESS)
+ *98 if insufficient memory was available (FAILURE)
  */
 
 void *malloc_checked(unsigned int b)
